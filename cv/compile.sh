746685dcce59ca4ec1c9c2356eea5686bb5ec9a3
OEM Configuration (temporary user) <you@example.com>
2015-12-28 21:17:51 +0100

    CV with laTex: add : latex install help

--- cv/compile.sh
+++ cv/compile.sh
@@ -1,3 +1,7 @@
-
+## http://www-rohan.sdsu.edu/~aty/bibliog/latex/LaTeXtoPDF.html
+## sudo apt-get install texlive texlive-base texlive-base-bin texlive-common texlive-font-utils texlive-fonts-extra texlive-fonts-recommended texlive-humanities texlive-lang-cyrillic texlive-lang-french texlive-lang-latin texlive-latex-base texlive-latex-extra texlive-latex-recommended texlive-math-extra texlive-pictures texlive-pstricks
+## sudo apt-get install texlive texlive-base texlive-base-bin texlive-font-utils texlive-fonts-extra texlive-fonts-recommended texlive-humanities texlive-lang-cyrillic texlive-lang-french texlive-lang-latin texlive-latex-base texlive-latex-extra texlive-latex-recommended texlive-math-extra texlive-pictures texlive-pstricks
+## sudo apt-get install texlive-fonts-recommended latex-beamer texpower texlive-pictures texlive-latex-extra imagemagick
+## sudo apt-get install texlive-latex-extra python-pygments
 pdflatex alexa_cv_hun.tex
 pdflatex alexa_cv_eng.tex
