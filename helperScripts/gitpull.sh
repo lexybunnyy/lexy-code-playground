06486fad92c3aea334f1a96e78100a89eebf5162
OEM Configuration (temporary user) <you@example.com>
2016-06-26 20:00:12 +0200

    update git commands - log the current state

--- helperScripts/gitpull.sh
+++ helperScripts/gitpull.sh
@@ -1,19 +1,34 @@
-## chmod +x gitpull.sh gitcommit.sh gitclone.sh
 ## origin	https://github.com/lexybunnyy/lexycode.git
 ## origin	https://github.com/lexybunnyy/lexythesis.git
 ## origin	git@nyilas.net:adogyalu 
 ## origin	git@nyilas.net:lexybunnyy
 ## git clone git@ideapp.hu:adogyalu
+git clone git@nyilas.net:adogyalu
+git clone git@nyilas.net:lexybunnyy 
+git clone https://github.com/lexybunnyy/lexycode.git
+git clone https://github.com/lexybunnyy/lexythesis.git
+echo ......start adogyalu
+
 cd adogyalu
 git pull
+git status
+echo ...... start lexybunnyy
+
 cd ../lexybunnyy
 git pull
+git status
+
+echo ...... start lexycode
 cd ../lexycode
 git pull
+git status
+
+echo ...... start lexythesis 
 cd ../lexythesis
 git pull
-cd ../wedding
-git pull
-cd ..
+git status
+echo ......
 echo updates_done
+
+
 
